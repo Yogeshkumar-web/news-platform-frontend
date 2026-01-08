c097f7fdee9466489c742d87a7a24c26f8ad1c1c
YogeshKumar-web <yogeshkumarwebdev@gmail.com>
2026-01-09 02:53:10 +0530

    ESlint removed

--- next.config.ts
+++ next.config.ts
@@ -33,11 +33,11 @@ const securityHeaders = [
 
 const nextConfig: NextConfig = {
   // ... other configs
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: false,
-  },
+  // eslint: {
+  //   // Warning: This allows production builds to successfully complete even if
+  //   // your project has ESLint errors.
+  //   ignoreDuringBuilds: false,
+  // },
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
